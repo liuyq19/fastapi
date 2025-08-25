326d857a56456d4f5fb2bfa4367c895250485281
jarvisyqliu <jarvisyqliu@tencent.com>
2025-08-25 20:47:42 +0800

    202508252047

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Annotated, Literal
 
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    print(item.dict(), type(item.dict()))
-    # print(**item.dict)
-    func(**item.dict())
-    return {"item_id": item_id, **item.dict()}
+class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
 
 
-def func(name,description,price,tax):
-    print(name,description,price,tax)
+@app.get("/items/")
+async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    return filter_query
 
 if __name__ == "__main__":
 
